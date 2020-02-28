46c705ccc1b1e8231b92d45fc32d8e847f6f43a3
Jake <panamajake86@gmail.com>
2020-02-27 21:21:06 -0700

    trying to make this shit work.

--- routers/user-router.js
+++ routers/user-router.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 const Users = require('../models/users-model');
 const restricted = require('../middleware/restricted-middleware');
-
+ 
 router.get('/users', restricted, (req, res) => {
     Users.find()
-        .then(users => {
-            res.json(users);
+        .then(user => {
+            res.json(user);
         })
         .catch(err => res.send({ message: "Try again later.", err }));
 });
