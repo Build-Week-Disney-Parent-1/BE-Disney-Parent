c821dc4e6757267739b3ec9c54e7add73e4edbe4
Jake <panamajake86@gmail.com>
2020-02-27 20:25:47 -0700

    fixed the seed file to make sure everything works correctly.

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   return knex('users').insert([
-    {name: 'Walt Disney', email: 'walt@email.com', password: 'password', role: "parent"},
-    {name: 'Ub Iwerks', email: 'ub@email.com', password: 'password', role: "parent"},
-    {name: 'Mickey Mouse', email: 'mickey@email.com', password: 'password', role: "parent"},
-    {name: 'Oswald Rabbit', email: 'oswald@email.com', password: 'password', role: "parent"}
-    {name: 'Child Care', email: 'child@email.com', password: 'password', role: "volunteer"},
-    {name: 'Care Taker', email: 'care@email.com', password: 'password', role: "volunteer"},
-    {name: 'Baby Sitter', email: 'baby@email.com', password: 'password', role: "volunteer"},
-    {name: 'Donald Duck', email: 'donald@email.com', password: 'password', role: "volunteer"}
+    {username: 'Walt Disney', email: 'walt@email.com', password: 'password', role: "parent"},
+    {username: 'Ub Iwerks', email: 'ub@email.com', password: 'password', role: "parent"},
+    {username: 'Mickey Mouse', email: 'mickey@email.com', password: 'password', role: "parent"},
+    {username: 'Oswald Rabbit', email: 'oswald@email.com', password: 'password', role: "parent"},
+    {username: 'Child Care', email: 'child@email.com', password: 'password', role: "volunteer"},
+    {username: 'Care Taker', email: 'care@email.com', password: 'password', role: "volunteer"},
+    {username: 'Baby Sitter', email: 'baby@email.com', password: 'password', role: "volunteer"},
+    {username: 'Donald Duck', email: 'donald@email.com', password: 'password', role: "volunteer"}
   ]);
 };
