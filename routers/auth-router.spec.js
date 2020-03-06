c50398b5e47549136207fac51225448ce70bc12d
Jake <panamajake86@gmail.com>
2020-03-06 12:11:38 -0700

    more testing

--- routers/auth-router.spec.js
+++ routers/auth-router.spec.js
@@ -1,36 +1,37 @@
 const request = require('supertest');
 const auth = require('../api/server');
 const db = require('../data/dbConfig');
-const User = require('../models/users-model');
+const Users = require('../models/users-model');
 const bc = require('bcryptjs');
 
 async function createUser(username, email, password, role) {
     const user = {
-        username: username,
-        email: email,
-        password: bc.hashSync(password, 8),
-        role: role
+        'username': username,
+        'email': email,
+        'password': bc.hashSync(password, 8),
+        'role': role
     };
 
-    await User.add(user);
+    await Users.add(user);
 }
 
-describe('POST register in AUTH', () => {
-    it('/register returns JSON', async () => {
-        const res = await request(auth)
-            .post('/api/auth/register')
-            .send({
-                "username": "brester",
-                "email": "test@test.com",
-                "password": "pass",
-                "role": "parent"
-            })
-            console.log(res.body);    
-            expect(res.type).toBe('application/json');
-            expect(res.status).toBe(201);
-    });
+describe('POST in AUTH', () => {
+    // it('/register returns JSON', async () => {
+    //     const res = await request(auth)
+    //         .post('/api/auth/register')
+    //         .send({
+    //             'username': 'tester',
+    //             'email': 'test@email',
+    //             'password': 'password',
+    //             'role': 'role'
+    //         })
+    //         console.log(res);    
+    //         expect(res.type).toBe('application/json');
+    //         expect(res.status).toBe(201);
+    // });
 
     it('/login returns JSON', async () => {
+        await createUser('tester', 'test@test.com', 'pass', 'parent')
         const res = await request(auth)
             .post('/api/auth/login')
             .send({
@@ -38,9 +39,9 @@ describe('POST register in AUTH', () => {
                 "password": "pass",
                 "role": "parent"
             })
-            console.log(res.body);    
+            // console.log(res.body);
             expect(res.type).toBe('application/json');
-            expect(res.status).toBe(201);
+            expect(res.status).toBe(200);
     });
 });
 
--- routers/user-router.spec.js
+++ routers/user-router.spec.js
@@ -1,1 +1,34 @@
-test.todo('not set up yet');
+const request = require('supertest');
+const user = require('../api/server');
+const db = require('../data/dbConfig');
+const Users = require('../models/users-model');
+const bc = require('bcryptjs');
+
+async function createUser(username, email, password, role) {
+    const user = {
+        'username': username,
+        'email': email,
+        'password': bc.hashSync(password, 8),
+        'role': role
+    };
+
+    await User.add(user);
+}
+
+describe('GET in USER', () => {
+    it('/ is protected', async () => {
+        const res = await request(user)
+            .get('/api/users');
+            expect(res.body.message).toBe('You need to sign in!');
+    });
+
+    it('/ returns ', async () => {
+        await createUser('John Doe', 'john@doe.com', 'pass', 'parent');
+        const
+    });
+});
+
+
+beforeEach( async () => {
+    await db ('users').truncate();
+});
